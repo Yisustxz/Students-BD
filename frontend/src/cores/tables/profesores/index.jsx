09f60490982c0f894ef12b282509a9a54775b704
Alejandro <alejandrojrosas2001@gmail.com>
2023-06-19 22:06:51 -0400

    mini boton

--- frontend/src/cores/tables/profesores/index.jsx
+++ frontend/src/cores/tables/profesores/index.jsx
@@ -76,6 +76,13 @@ const Profesores = () => {
             Listado de Profesores
           </h1>
         </div>
+        <div className='flex justify-center'>
+          <Link className='flex w-full justify-center' to='/'>
+            <button className='text-violet-600 w-2/4 bg-[#f9f9f9] rounded-lg text-[1em] font-medium cursor-pointer px-[0.6em] py-[1em] transition-all border-2 hover:border-violet-400 mb-3 mr-8'>
+              Añadir Profesor
+            </button>
+          </Link>
+        </div>
         <table className='mx-24'>
           <thead>
             <tr className='bg-gray-200 text-gray-600 uppercase text-sm leading-normal'>
